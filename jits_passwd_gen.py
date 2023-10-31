9113a32856d0d907fc1ecc3e25b1b78ef844ed3a
Jithu T Narayanan <105491982+jithunarayanan@users.noreply.github.com>
2023-10-31 18:40:24 +0400

    Update jits_passwd_gen.py

--- jits_passwd_gen.py
+++ jits_passwd_gen.py
@@ -3,7 +3,7 @@ import random
 lettersu = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 lettersl = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '*', '?', '<', '>',]
+symbols = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '*', '?', '<', '>']
 
 greeting = "Thank you for using Jit's passwd generator🎲"
 passlist = [lettersu, lettersl, numbers, symbols]
@@ -60,3 +60,6 @@ while True:
         break
     else:
         print("Your selection is invalid***. Please select correct input,")
+
+
+##Author: JithuNarayanan
